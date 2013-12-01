ad0dd138cfb58dd1dfda6bda3c7be3c995d9abfc
Chris Trotman <chris@trotman.io>
2013-12-01 21:07:42 +1100

    Update the keyboard layout to my personal preferences.

--- keyboard/teck/keymap.c
+++ keyboard/teck/keymap.c
@@ -80,23 +80,23 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Not sure what the key on the left of '~' is supposed to be. The key is C2,R2. */
     /* 0: qwerty */
     KEYMAP_ANSI(
-        ESC,    F1,     F2,     F3,     F4,     F5,     F6,     F7,     F8,     F9,     F10,    F11,    F12, \
-        NO,     ESC,    BSLS,   NO,     NO,     CAPS,   FN0,    NLCK,   NO,     NO,     NO,     EQL,    RBRC, \
+        ESC,    F1,     F2,     F3,     F4,     F5,     F6,     MPRV,   MPLY,   MNXT,   MUTE,   VOLD,    VOLU, \
+        NO,     ESC,    BSLS,   NO,     NO,     NO,     FN0,    NLCK,   NO,     NO,     NO,     EQL,    RBRC, \
         GRV,    1,      2,      3,      4,      5,      LGUI,   6,      7,      8,      9,      0,      MINS, \
         SLASH,  Q,      W,      E,      R,      T,      DEL,    Y,      U,      I,      O,      P,      LBRC, \
         LSFT,   A,      S,      D,      F,      G,      TAB,    H,      J,      K,      L,      SCLN,   RSFT, \
-        LCTRL,  Z,      X,      C,      V,      B,      BSPC,   N,      M,      COMM,   DOT,    QUOT,   RCTRL, \
-        LALT,   HOME,   PGDN,   PGUP,   END,    SPC,    ENT,    SPC,    LEFT,   DOWN,   UP,     RIGHT,  RALT  \
+        TAB,    Z,      X,      C,      V,      B,      BSPC,   N,      M,      COMM,   DOT,    QUOT,   ENT, \
+        LCTRL,  HOME,   PGDN,   PGUP,   END,    BSPC,   LGUI,   SPC,    LEFT,   DOWN,   UP,     RIGHT,  RALT  \
     ),
     /* 1: first FN layer */
     KEYMAP_ANSI(
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO, \
-        NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO,     NO  \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRMS,   TRNS,   F7,     F8,     F9,     F10,    F11,    F12, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   CAPS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS, \
+        TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS  \
     )
 };
 
